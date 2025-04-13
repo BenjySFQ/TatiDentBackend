6aa279e7c0791ee217b9738f59c4229adb52c95e
Benjy Smith Fernandez Quispe <benjamin77122@gmail.com>
2025-04-13 17:46:23 -0500

    Avanzed proyect

---
+++ src/main/java/com/benja/ben/dto/UsernameDto.java
@@ -0,0 +1,11 @@
+package com.benja.ben.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsernameDto {
+    private String userName;
+    private String password;
+}
